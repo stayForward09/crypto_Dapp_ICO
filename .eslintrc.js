65ca95e9ba56d84ee526b47aefb86985715ced2a
stayForward09 <daveescott0059@gmail.com>
2024-11-21 02:04:10 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    mocha: true,
+    node: true,
+  },
+  extends: ["productsway/react"],
+  ignorePatterns: ["dist", ".eslintrc.js", "vite.config.ts"],
+  parserOptions: {
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+  },
+  overrides: [
+    {
+      files: ["hardhat.config.js"],
+      globals: { task: true },
+    },
+    {
+      files: ["scripts/**"],
+      rules: { "no-process-exit": "off" },
+    },
+    {
+      files: ["hardhat.config.js", "scripts/**", "test/**"],
+      rules: { "node/no-unpublished-require": "off" },
+    },
+  ],
+};
